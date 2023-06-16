58775bea24e218d5e0bb0d9a0b7651c86e70974d
tomasjd08 <tomasgirma07@gmail.com>
2023-06-16 20:57:52 +0300

    first commit

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,15 +9,15 @@ int main(void)
 {
 		char ch = 'a';
 
-			while (ch <= 'z')
-					{
-								if (ch != 'e' && ch != 'q')
-											{
-															putchar(ch);
-																	}
-										ch++;
-											}
-				putchar('\n');
-					return (0);
+		while (ch <= 'z')
+		{
+			if (ch != 'e' && ch != 'q')
+			{
+				putchar(ch);
+			}
+			ch++;
+		}
+		putchar('\n');
+		return (0);
 }
 
