77bfbd9ef2e689e32c79ecba5d2c5660cf5854f7
tomasjd08 <tomasgirma07@gmail.com>
2023-06-17 01:47:23 +0300

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,21 @@
  */
 int main(void)
 	{
-			int d, p, q;
-			
-			for (d = '0'; d < '9'; d++)
+		int d, p, q;
+
+		for (d = '0'; d < '9'; d++)
 			{
-				for (p = d +1; p <= '9'; p++)
+				for (p = d + 1; p <= '9'; p++)
 				{
-					for (q = p +1; q <= '9'; q++)
+					for (q = p + 1; q <= '9'; q++)
 					{
-						if((p != d) != q)
+						if(p != d && p != q)
 						{
 							putchar(d);
 							putchar(p);
 							putchar(q);
-							if (d == '7' && p == '8')
+							if (d == '7')
+								if (p == '8')
 								continue;
 							putchar(',');
 							putchar(' ');
