94d3e8e798f675177bab2551819ce79199400eae
tomasjd08 <tomasgirma07@gmail.com>
2023-06-16 18:57:53 +0300

    first commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,11 @@
-#include ‹stido.h›
+#include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
 int main(void)
 {
 	int n;
@@ -26,4 +26,3 @@ int main(void)
 	}
 	return (0);
 }
-
