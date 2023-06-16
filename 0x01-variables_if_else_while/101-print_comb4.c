ad12f935cc71087d1ca5f6b2b6a31028bebe6322
tomasjd08 <tomasgirma07@gmail.com>
2023-06-16 22:00:49 +0300

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -20,10 +20,10 @@ int main(void)
 						putchar(m);
 						putchar(l);
 						if (n != 55 || m != 56)
-																	{
-																								putchar(',');
-																														putchar(' ');
-																																			}
+						{
+							putchar(',');
+							putchar(' ');
+						}
 
 					}
 				}
