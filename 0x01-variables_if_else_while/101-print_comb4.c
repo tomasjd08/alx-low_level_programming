8ccb5d1bb88b9c4cf00a6af102f9adfe20a12f11
tomasjd08 <tomasgirma07@gmail.com>
2023-06-17 02:45:45 +0300

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,12 +9,15 @@ int main(void)
 		int c;
 		int d;
 		int e = 0;
+
 		while (e < 10)
 		{
 			d = 0;
+			
 			while (d < 10)
 			{
 				c = 0;
+				
 				while (c < 10)
 				{
 					if (c != d && d != e && e < d && d < c)
