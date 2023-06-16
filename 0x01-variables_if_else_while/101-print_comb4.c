c4c03d485ef0a20d2dae6a0d0f6cd391aa0957aa
tomasjd08 <tomasgirma07@gmail.com>
2023-06-17 02:13:17 +0300

    first commit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -16,20 +16,19 @@ int main(void)
 
 					for (q = p + 1; q <= '9'; q++)
 					{
-						if ((p != d) != q) {
-							    putchar(d);
-							        putchar(p);
-								    putchar(q);
-								        if (d == '7' && p == '8') {
-										        continue;
-											    }                                                             
-									    putchar(',');
-									        putchar(' ');
+						if ((p != d) != q)
+						{
+							putchar(d);
+							putchar(p);
+							putchar(q);
+							if (d == '7' && p == '8')
+								continue;
+							putchar(',');
+							putchar(' ');
 						}
 					}
 				}
 			}
-			putchar('\n');
-			return (0);
+		putchar('\n');
+		return(0);
 	}
-
