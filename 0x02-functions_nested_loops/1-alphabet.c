523a80de0294ffe105e0aef2dec13ad01ea8c270
tomasjd08 <tomasgirma07@gmail.com>
2023-06-20 10:55:07 -0700

    2nd

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  * Description: 'the program's description'
  * Return: Always 0 (Success)
  */
+
 void print_alphabet(void)
 	{
 		char letter;
