9385c2b2aec7fe6c1a8cae42e3a06f70c3c8f892
tomasjd08 <tomasgirma07@gmail.com>
2023-06-20 10:40:56 -0700

    1st commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,10 @@
-#include "main.h"
-	/**
-	 * main - Entry point
-	 * Description: the program's print _putchar
-	 *
-	 * Return: 0
-	 */
-
+#include"main.h"
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ * Return: Always 0 (Success)
+ */
 
 	int main(void)
 	{
