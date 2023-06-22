f20dc909cd32fa81824ae4e1eb13531939df92df
tomasjd08 <tomasgirma07@gmail.com>
2023-06-22 22:34:54 +0300

    hey there

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,28 +8,33 @@ nclude <stdio.h>
  */
 int main(void)
 {
-	long int n;
-	long int max;
-	long int i;
+		long int n;
+			long int max;
+				long int i;
 
-	n = 612852475143;
-	max = -1;
+					n = 612852475143;
+						max = -1;
 
-	while (n % 2 == 0)
-	{
-		max = 2;
-		n /= 2;
-	}
-	for (i = 3; i <= sqrt(n); i = i + 2)
-	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-	}
-	if (n > 2)
-		max = n;
-	printf("%ld\n", max)
-		return (0);
+							while (n % 2 == 0)
+									{
+												max = 2;
+														n /= 2;
+															}
+
+								for (i = 3; i <= sqrt(n); i = i + 2)
+										{
+													while (n % i == 0)
+																{
+																				max = i;
+																							n = n / i;
+																									}
+														}
+
+									if (n > 2)
+												max = n;
+
+										printf("%ld\n", max);
+
+											return (0);
 }
+
