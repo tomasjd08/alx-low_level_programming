c4317ba39ab6098891fc788c8f5787a24cc4e9fb
tomasjd08 <tomasgirma07@gmail.com>
2023-07-17 11:51:45 -0700

    commit

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,12 +1,12 @@
-#include <stdlib.h> 
-#include "dog.h" 
-  
- /** 
-  * free_dog - frees memory allocated for a struct dog 
-  * @d: struct dog to free 
-  */ 
- void free_dog(dog_t *d)
-{ 
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * free_dog - frees memory allocated for a struct dog
+ * @d: struct dog to free
+ */
+void free_dog(dog_t *d)
+{
 	if (d)
 	{
 		free(d->name);
