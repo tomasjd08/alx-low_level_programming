dad38536aae4e7be81579e28526d1a36bd4aae15
tomasjd08 <tomasgirma07@gmail.com>
2023-07-17 11:38:18 -0700

    commit

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/** 
- * main - prints the name of the file 
- * 
- * Return: Always 0 (Success) 
+
+/**
+ * main - prints the name of the file
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
